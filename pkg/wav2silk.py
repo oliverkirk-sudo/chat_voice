ace3759146e11b314a27e18616247de5990c6dff
oliverkirk-sudo <78022033+oliverkirk-sudo@users.noreply.github.com>
2023-03-17 09:50:06 +0800

    修改采样率默认值

--- pkg/wav2silk.py
+++ pkg/wav2silk.py
@@ -8,7 +8,7 @@ def convert_to_silk(media_path: str) -> str:
     pcm_path = os.path.dirname(media_path)
     silk_path = os.path.join(pcm_path, (name + '.silk'))
     pcm_path = os.path.join(pcm_path, (name + '.pcm'))
-    media.export(pcm_path, 's16le', parameters=['-ar', str(32000), '-ac', '1']).close()
-    pilk.encode(pcm_path, silk_path, pcm_rate=32000, tencent=True)
+    media.export(pcm_path, 's16le', parameters=['-ar', str(24000), '-ac', '1']).close()
+    pilk.encode(pcm_path, silk_path, pcm_rate=24000, tencent=True)
     logging.info('silk生成成功')
     return silk_path
